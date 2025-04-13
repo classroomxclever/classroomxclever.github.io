fd23ac58d666c0e2915852bf6f613a3acc0c291c
Classroom-X-Clever <pumatagyt@gmail.com>
2025-04-12 22:26:58 -0400

    e

---
+++ secret.js
@@ -0,0 +1,70 @@
+// secret.js
+
+const canvas = document.getElementById("starCanvas");
+const ctx = canvas.getContext("2d");
+
+let stars = [];
+const numStars = 200;
+let mouse = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+resizeCanvas();
+window.addEventListener("resize", resizeCanvas);
+
+document.addEventListener("mousemove", (e) => {
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+});
+
+class Star {
+  constructor() {
+    this.reset();
+  }
+
+  reset() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 1.5;
+    this.speedX = (Math.random() - 0.5) * 0.5;
+    this.speedY = (Math.random() - 0.5) * 0.5;
+  }
+
+  update() {
+    this.x += this.speedX + (mouse.x - canvas.width / 2) * 0.00005;
+    this.y += this.speedY + (mouse.y - canvas.height / 2) * 0.00005;
+
+    if (
+      this.x < 0 ||
+      this.x > canvas.width ||
+      this.y < 0 ||
+      this.y > canvas.height
+    ) {
+      this.reset();
+    }
+  }
+
+  draw() {
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+for (let i = 0; i < numStars; i++) {
+  stars.push(new Star());
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let star of stars) {
+    star.update();
+    star.draw();
+  }
+  requestAnimationFrame(animate);
+}
+
+animate();
